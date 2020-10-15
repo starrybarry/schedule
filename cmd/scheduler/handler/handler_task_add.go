ce421648e1fa2242a80167d3013389f4919cf54f
starry <kinlus.workspace@gmail.com>
2020-10-15 22:58:27 +0300

    handler: add correct response

--- cmd/scheduler/handler/handler_task_add.go
+++ cmd/scheduler/handler/handler_task_add.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 
+	jsoniter "github.com/json-iterator/go"
+
 	"github.com/starrybarry/schedule/pkg/scheduler"
 
 	"go.uber.org/zap"
@@ -15,16 +17,25 @@ func (h *handler) AddTask(w http.ResponseWriter, r *http.Request) {
 
 	task, err := h.extractor.ExtractTask(r)
 	if err != nil {
-		_ = scheduler.NewResponse(false)
+		h.failed(w, scheduler.NewResponse(false), http.StatusBadRequest)
 		h.log.Error("extract task", zap.Error(err))
+		return
 	}
 
 	if err := h.schedulerSV.AddTask(ctx, task); err != nil {
-		_ = scheduler.NewResponse(false)
+		h.failed(w, scheduler.NewResponse(false), http.StatusInternalServerError)
 		h.log.Error("add task", zap.Error(err))
+		return
 	}
 
-	_ = scheduler.NewResponse(true)
+	b, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(scheduler.NewResponse(true))
+	if err != nil {
+		h.failed(w, scheduler.NewResponse(false), http.StatusInternalServerError)
+		h.log.Error("marshal response", zap.Error(err))
+		return
+	}
+
+	h.success(w, b)
 
 	h.log.Info("add task complete!")
 }
--- cmd/scheduler/handler/handler_task_delete.go
+++ cmd/scheduler/handler/handler_task_delete.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"net/http"
 
+	jsoniter "github.com/json-iterator/go"
+
 	"github.com/starrybarry/schedule/pkg/scheduler"
 	"go.uber.org/zap"
 )
@@ -14,16 +16,25 @@ func (h *handler) DeleteTask(w http.ResponseWriter, r *http.Request) {
 
 	id, err := h.extractor.ExtractTaskID(r)
 	if err != nil {
-		_ = scheduler.NewResponse(false)
+		h.failed(w, scheduler.NewResponse(false), http.StatusBadRequest)
 		h.log.Error("extract id", zap.Error(err))
+		return
 	}
 
 	if err := h.schedulerSV.DeleteTask(ctx, scheduler.Task{ID: id}); err != nil {
-		_ = scheduler.NewResponse(false)
+		h.failed(w, scheduler.NewResponse(false), http.StatusInternalServerError)
 		h.log.Error("delete task", zap.Error(err))
+		return
 	}
 
-	_ = scheduler.NewResponse(true)
+	b, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(scheduler.NewResponse(true))
+	if err != nil {
+		h.failed(w, scheduler.NewResponse(false), http.StatusInternalServerError)
+		h.log.Error("marshal response", zap.Error(err))
+		return
+	}
+
+	h.success(w, b)
 
 	h.log.Info("delete task complete!")
 }
